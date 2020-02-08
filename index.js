084eaec82f6f9a89106f3e6e8cdab635b9d843b5
Michael Barton <mikebarton7891@gmail.com>
2020-02-08 17:55:53 -0600

    Bug fix

--- index.js
+++ index.js
@@ -52,11 +52,12 @@ client.on('message', async (message) => {
         userLimit = args[1];
     }
 
-    if (args.length >= 2 && !(parseInt(args[2]) == args[2])) {
+    if (args.length >= 3 && !(parseInt(args[2]) == args[2])) {
         // if passed more arguments than expected and the second argument is not a number
         // then the user didn't escape the channel name with quotes
         // we will treat it like ALL arguments are apart of the channel name
         channelName = args.slice(1).join(" ");
+        userLimit = null;
     }
 
     if (parseInt(userLimit) > 99) userLimit = 99;
